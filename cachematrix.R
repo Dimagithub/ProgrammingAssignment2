0fc515bba754aefefd5e8ff8983ad89add4bb160
demodima2\dima <dima@semensky.com>
2015-08-30 16:45:58 -0400

    Completed

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## create "object" of a type MATRIX
+# add four "methods" - get, set, getinv, setinv
+# parameter - may be skipped or existing matrix
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(mt = matrix()) {
+  mt.inverse <- NULL
+  set <- function(y) {
+    mt <<- y
+    mt.inverse <<- NULL
+  }
+  get <- function() mt
+  setinv <- function(inv) mt.inverse <<- inv
+  getinv <- function() mt.inverse
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
-
+## Returns inverse of the parameter matrix
+# assumption - the given matrix is invertible
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinv()
+  #check if cache does not exist - then create it
+  if (is.null(inv)){
+    message("creating cache first time")
+    mat <- x$get()
+    inv <- solve(mat)
+    x$setinv(inv)
+  }
+  inv
 }
